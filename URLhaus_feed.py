c69d301dfc2d27e4cb411082030707438b829f2a
z060404z <62229969+z060404z@users.noreply.github.com>
2025-09-01 20:57:51 +0800

    URLhaus_feed.py

--- URLhaus_feed.py
+++ URLhaus_feed.py
@@ -1,13 +1,17 @@
 import requests
 import csv
+import sys
 
-# URLhaus CSV 最近惡意 URL
 csv_url = "https://urlhaus.abuse.ch/downloads/csv_recent/"
 
-resp = requests.get(csv_url)
-resp.raise_for_status()
-lines = resp.text.splitlines()
+try:
+    resp = requests.get(csv_url)
+    resp.raise_for_status()
+except Exception as e:
+    print("抓取 CSV 失敗:", e)
+    sys.exit(1)
 
+lines = resp.text.splitlines()
 reader = csv.DictReader(lines)
 
 with open("URLhaus_feed.txt", "w", encoding="utf-8") as f:
